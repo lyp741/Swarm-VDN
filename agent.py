c69b6947bb9c2674454be3356c2ea725a9598c28
lyp741 <liangyupeng13@gmail.com>
2019-07-05 17:05:49 +0900

    bug fixes

--- agent.py
+++ agent.py
@@ -5,17 +5,25 @@ from buffer import ReplayBuffer
 import torch.nn as nn
 import torch.optim as optim
 import random
+import datetime
+import os
 
 class Agent():
-    def __init__(self):
-        self.buffer_size = 10000
+    def __init__(self, args):
+        self.buffer_size =5000
         self.batch_size = 32
         self.num_agents = 0
         self.num_of_actions = 8
         self.model = []
         self.buffer = []
         self.time = 0
-        self.gamma = 0.99
+        self.gamma = 0.9
+        self.args = args
+        self.time_now = datetime.datetime.now().strftime('%Y-%m-%d')
+        try:
+            os.mkdir(self.time_now)
+        except:
+            pass
 
     def init(self, obs):
         self.device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
@@ -23,9 +31,10 @@ class Agent():
         self.num_agents = len(obs['image'])
         self.buffer = ReplayBuffer(self.buffer_size, self.batch_size, self.num_agents)
         self.model = VDN(self.num_agents, self.num_of_actions, self.device).to(self.device)
+        if self.args.model != 'None':
+            self.load_model(self.args.model)
         self.target = VDN(self.num_agents, self.num_of_actions, self.device).to(self.device)
         self.update_target()
-        print(self.model.parameters)
         self.optimizer = optim.Adam(self.model.parameters())
         self.last_state_cnn = np.zeros((self.num_agents,3,128,128))
         self.last_state_oth = np.zeros((self.num_agents, 11))
@@ -37,6 +46,7 @@ class Agent():
             temp.append(np.r_[obs["image"][i]])
         temp = np.r_[temp]
         t = np.transpose(temp, (0,3,1,2))
+        # t /= 255.0
         return t
 
     def get_obs_oth(self, obs):
@@ -67,17 +77,13 @@ class Agent():
         state_oth = self.get_obs_oth(obs)
         cat_cnn = self.get_new_cnn(state_cnn)
         cat_oth = self.get_new_oth(state_oth)
-        q, actions = self.model(cat_cnn,cat_oth)
+        q, actions, _ = self.model(cat_cnn,cat_oth)
         index_action = np.zeros((self.num_agents,), dtype=np.uint8)
         for i in range(self.num_agents):
             if random.random() > epsilon:
-                index_action[i] = random.randint(0, self.num_of_actions-1)
-                if index_action[i] == 8:
-                    print('action 8 in random')
+                index_action[i] = random.randint(0, self.num_of_actions - 1)
             else:
                 index_action[i] = actions[0,i].item()
-                if index_action[i] == 8:
-                    print('action 8 in output')
 
         if done.item(0) != True:
             self.last_state_cnn = state_cnn
@@ -93,26 +99,33 @@ class Agent():
         self.time += 1
         if len(self.buffer) < self.batch_size:
             return
-        for i in range(3):
-            state_cnn, state_oth, action, reward, next_cnn, next_oth, done = self.buffer.sample()
 
-            max_q = self.target(next_cnn, next_oth)[0].detach()
-            pred_q, _ = self.model(state_cnn, state_oth)
-            reward = reward.sum(1)
-            true_q = reward + (1 - done[:,0]) * self.gamma * max_q
-            criterion = nn.MSELoss()
-            loss = criterion(pred_q, true_q)
-            self.optimizer.zero_grad()
-            loss.backward()
-            self.optimizer.step()
-        print(loss.detach().item())
+        state_cnn, state_oth, action, reward, next_cnn, next_oth, done = self.buffer.sample()
 
-        
-
+        max_q = self.target(next_cnn, next_oth)[0].detach()
+        _, _,  pred_q = self.model(state_cnn, state_oth, action)
+        reward = reward.sum(1)
+        true_q = reward + (1 - done[:,0]) * self.gamma * max_q
+        criterion = nn.MSELoss()
+        loss = criterion(pred_q, true_q)
+        self.optimizer.zero_grad()
+        loss.backward()
+        self.optimizer.step()
         if self.time % 10 == 0:
             self.update_target()
+        if self.time % 100 == 0:
+            self.save_model(self.time_now + '/' + str(self.num_agents) + '_' + str(self.time))
 
     def store_experience(self, obs, action, reward, done):
         state_cnn = self.get_obs_cnn(obs)
         state_oth = self.get_obs_oth(obs)
         self.buffer.add(state_cnn, state_oth, action, reward, done)
+
+    def save_model(self, filename):
+        # filename = './' + str(self.num_agents)
+        torch.save(self.model.state_dict(), filename)
+
+    def load_model(self, filename):
+        # filename = './' + self.num_agents
+        self.model.load_state_dict(torch.load(filename))
+        self.model.eval()
